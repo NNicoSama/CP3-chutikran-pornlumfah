4cc22dbb76db0ddc05cf384455fb4931277c27f2
NNicoSama <popnoon00@gmail.com>
2022-01-22 11:09:35 +0700

    Exercise4

---
+++ Exercise4_Chutikran_P.py
@@ -0,0 +1,9 @@
+FE = 50
+GB = 40
+ICS = 60
+CP = 70
+print("---Your Score---")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems :",ICS)
+print("Computer Programming :",CP)
